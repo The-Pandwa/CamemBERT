5488c52be72f59d83f008f24c8baf833175dd714
The-Pandwa <152164692+The-Pandwa@users.noreply.github.com>
2024-09-09 18:01:58 +0200

    Update Code.py

--- Code.py
+++ Code.py
@@ -26,8 +26,8 @@ enc_test = encode_sentence(test_phrase)
 similarity1 = st.write(torch.nn.functional.cosine_similarity(enc_test, enc1))
 similarity2 = st.write(torch.nn.functional.cosine_similarity(enc_test, enc2))
 
-print(f"Similarité phrase 1: {similarity1.item()}")
-print(f"Similarité phrase 2: {similarity2.item()}")
+st.write(f"Similarité phrase 1: {similarity1.item()}")
+st.write(f"Similarité phrase 2: {similarity2.item()}")
 
 st.title('Titre')
 st.header('En tête')

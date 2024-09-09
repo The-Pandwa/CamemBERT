25c9e4f3d050166216f602de30f1a1c924084248
The-Pandwa <152164692+The-Pandwa@users.noreply.github.com>
2024-09-09 18:20:23 +0200

    Update Code.py

--- Code.py
+++ Code.py
@@ -15,22 +15,25 @@ def encode_sentence(sentence):
 # Tes phrases
 phrase1 = "Le chat mange une souris."
 phrase2 = "Le chien dort dans le jardin."
-test_phrase = st.text_area("Tape le texte","")
 
 # Encoder les phrases
 enc1 = encode_sentence(phrase1)
 enc2 = encode_sentence(phrase2)
 enc_test = encode_sentence(test_phrase)
 
+# Titre du streamlit
+st.title('Comparateur et similitude entre idées')
+
+# Explication du fonctionnement
+st.header('Début des tests !')
+
+# Zone d'expression libre
+test_phrase = st.text_area("Tape le texte","")
+
 # Calculer la similarité (cosine similarity)
 similarity1 = torch.nn.functional.cosine_similarity(enc_test, enc1)
 similarity2 = torch.nn.functional.cosine_similarity(enc_test, enc2)
 
-
-st.title('Comparateur et similitude entre idées')
-
-st.header('Début des tests !')
-
-
+# Résultat de la comparaison
 st.write(f"Similarité avec phrase 1 ('{phrase1}'): {similarity1.item():.4f}")
 st.write(f"Similarité avec phrase 2 ('{phrase2}'): {similarity2.item():.4f}")

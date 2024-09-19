43e9cfb8d4e6ab994aab648d9f6ca51fb0ed9a32
The-Pandwa <152164692+The-Pandwa@users.noreply.github.com>
2024-09-19 20:52:13 +0200

    test

--- Code.py
+++ Code.py
@@ -4,11 +4,7 @@ from transformers import CamembertModel, CamembertTokenizer
 import torch
 import torch.nn.functional as F
 import nltk
-from nltk.corpus import stopwords
 from nltk.tokenize import word_tokenize
-nltk.download('stopwords')
-nltk.download('punkt')
-nltk.download('punkt_tab')
 
 # Charger le modèle et le tokenizer CamemBERT
 model = CamembertModel.from_pretrained("camembert/camembert-base-ccnet-4gb")
@@ -19,13 +15,6 @@ def phrases_encoder(phrase):
     inputs = tokenizer(phrase, return_tensors="pt", padding=True, truncation=True)
     outputs = model(**inputs)
     return outputs.last_hidden_state.mean(dim=1)
-
-# Fonction pour supprimer les stop words
-def suppr_stopwords(text):
-    stop_words = set(stopwords.words('french'))
-    words = word_tokenize(text, language='french')
-    filtered_words = [word for word in words if word.lower() not in stop_words]
-    return ' '.join(filtered_words)
 
 # Dataframe
 df = pd.read_csv("The-Pandwa/CamemBERT/Perso.csv")
@@ -40,18 +29,14 @@ st.header('Début des tests !')
 # Zone d'expression libre
 test_phrase = st.text_area("Tape le texte","")
 
-# Supprimer les stop words de la phrase de test
-cleaned_test_phrase = suppr_stopwords(test_phrase)
-
 # Initialisation du dictionnaire des similarités
 similarities_dict = {}
 
 # Encoder la phrase de test
-encoded_test = phrases_encoder(cleaned_test_phrase)
+encoded_test = phrases_encoder(test_phrase)
 
 # Boucle pour encoder les phrases et les ajouter dans similarities_dict
-for clef, valeur in dict_perso.items():
-    cleaned_value = suppr_stopwords(valeur)
+for clef, valeur in df.items():
     encoded_value = phrases_encoder(valeur)  
     enc_clef = "enc_" + clef
     similarity = F.cosine_similarity(encoded_test, encoded_value).item() 

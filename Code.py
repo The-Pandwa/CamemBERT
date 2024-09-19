ca13998d02e13f4134d5a95bc5021fdabf642045
The-Pandwa <152164692+The-Pandwa@users.noreply.github.com>
2024-09-19 20:54:58 +0200

    test

--- Code.py
+++ Code.py
@@ -11,20 +11,16 @@ model = CamembertModel.from_pretrained("camembert/camembert-base-ccnet-4gb")
 tokenizer = CamembertTokenizer.from_pretrained("camembert/camembert-base-ccnet-4gb")
 
 # Fonction pour encoder une phrase
-def phrases_encoder(phrase):
-    inputs = tokenizer(phrase, return_tensors="pt", padding=True, truncation=True)
-    outputs = model(**inputs)
-    return outputs.last_hidden_state.mean(dim=1)
-
-# Dataframe
-df = pd.read_csv("The-Pandwa/CamemBERT/Perso.csv")
-st.dataframe(df)
 
 # Titre du streamlit
 st.title('Comparateur et similitude entre idées')
 
 # Explication du fonctionnement
 st.header('Début des tests !')
+
+# Dataframe
+df = pd.read_csv("The-Pandwa/CamemBERT/Perso.csv")
+st.dataframe(df)
 
 # Zone d'expression libre
 test_phrase = st.text_area("Tape le texte","")
@@ -33,27 +29,14 @@ test_phrase = st.text_area("Tape le texte","")
 similarities_dict = {}
 
 # Encoder la phrase de test
-encoded_test = phrases_encoder(test_phrase)
 
-# Boucle pour encoder les phrases et les ajouter dans similarities_dict
-for clef, valeur in df.items():
-    encoded_value = phrases_encoder(valeur)  
-    enc_clef = "enc_" + clef
-    similarity = F.cosine_similarity(encoded_test, encoded_value).item() 
-    similarities_dict[enc_clef] = similarity
-    st.write(f"Similarité avec '{clef}': {similarity:.4f}")
+# Boucle pour encoder les phrases
 
 # Trouver la phrase avec la similarité maximale
-max_phrase = max(similarities_dict, key=similarities_dict.get)
-max_similarity = similarities_dict[max_phrase]
 
 # Afficher la phrase correspondante avec la similarité maximale
-st.write(f"Similarité avec phrase '{max_phrase}': {max_similarity:.4f}")
 
 # Afficher le top 3 des meilleures correspondances
-top_3 = dict(sorted(similarities_dict.items(), key=lambda item: item[1], reverse=True)[2:3])
-top_2 = dict(sorted(similarities_dict.items(), key=lambda item: item[1], reverse=True)[1:2])
-top_1 = dict(sorted(similarities_dict.items(), key=lambda item: item[1], reverse=True)[0:1])
 
 # Organiser les résultats en colonne
 col1, col2, col3 = st.columns(3)
